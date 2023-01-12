32f840de7c9a6d161a512d36f606f22237a13862
Jason Zhang <zjjc123@hotmail.com>
2023-01-12 00:18:51 -0800

    Setting up TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs font-bold">
         Click on the Vite and React logos to learn more
       </p>
     </div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [process.env.NODE_ENV === "production" ? { cssnano: {} } : {}],
+};
