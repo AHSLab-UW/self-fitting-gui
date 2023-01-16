14430692654c36e271fcab796b2954f6289bd592
Jason Zhang <zjjc123@hotmail.com>
2023-01-16 15:34:08 -0800

    Basename for Deployment

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
