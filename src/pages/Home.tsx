604d659614ca6990e5bdd6ecbbe27bf488b05885
Jason Zhang <zjjc123@hotmail.com>
2023-01-23 13:35:26 -0800

    Command demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Home() {
+  const history = useLocation();
   return (
     <>
-      <div>Home</div>
+      <div>Home!!</div>
       <Link to="/drag-drop-demo">Drag Drop Demo</Link>
+      <div></div>
+      <form onSubmit={(e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+          command: { value: string };
+        };
+        fetch(`/device?command=${target.command.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+           console.log(data);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+      }}>
+        <input type="text" name="command"/>
+        <button type="submit">Send</button>
+      </form>
     </>
   )
 }
