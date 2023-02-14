190ca312cdb8ae84c1635b1956a46206edc755f9
Jason Zhang <zjjc123@hotmail.com>
2023-02-14 14:02:59 -0800

    Components Checkpoint (#8)
    * Default Progress Bar
    
    * draft 1 done, all function works
    
    draft 1 done, all function works
    
    * Co-authored-by: zjjc123@hotmail.com <zjjc123@hotmail.com>
    
    * 2/14
    
    ---------
    
    Co-authored-by: ZITONG0423 <zitongli@uw.edu>

---
+++ src/components/CollapsingSidebar.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import "./CollapsingSidebar.css";
+import closeIcon from '../assets/imgs/close.png'
+
+interface Props {
+    open: boolean,
+    closeModal: Function
+}
+
+const Dropdown = (props: Props) => {
+  let className = "sidebar"
+  if (props.open) {
+    className += " open"
+  }
+
+  return (
+    <div className={className}>
+        <div className="sidebar-header">
+            {/* Text on left, close icon on right */}
+            <div className="space-between">
+                <h2>Collapsing Sidebar</h2>
+                <img src={closeIcon} alt="Close sidebar" className='icon' onClick={() => props.closeModal()} />
+            </div>
+        </div>
+        <div className="sidebar-body">
+            {/* Body stuff goes here */}
+            <button>Button 1</button>
+            <div></div>
+            <button>Button 2</button>
+            <div></div>
+            <button>Button 3</button>
+            <div></div>
+        </div>
+        <div className="sidebar-footer">
+            {/* Footer stuff goes here */}
+        </div>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./ProgressBar.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+
+interface Props {
+  step: number
+}
+
+const MultiStepProgressBar = ({ step } : Props) => {
+  const stepPercentage = (step / 4) * 100;
+
+  return (
+    <ProgressBar percent={stepPercentage}>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+    </ProgressBar>
+  );
+};
+
+export default MultiStepProgressBar;
