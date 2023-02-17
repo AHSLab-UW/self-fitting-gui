f0f70bb7778ae9907e556998852828c4f82bd812
ZITONG0423 <81598813+ZITONG0423@users.noreply.github.com>
2023-02-17 14:29:11 -0800

    image slider function

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Slider.css";
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface SliderProps {
+  images: Image[];
+}
+
+const Slider: React.FC<SliderProps> = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    const nextIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const prevSlide = () => {
+    const prevIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(prevIndex);
+  };
+
+  return (
+    <div className="slider">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className={index === currentIndex ? "slide active" : "slide"}
+          src={image.src}
+          alt={image.alt}
+        />
+      ))}
+      <button className="prev" onClick={prevSlide}>
+        &#10094;
+      </button>
+      <button className="next" onClick={nextSlide}>
+        &#10095;
+      </button>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Intro1.tsx
+++ src/pages/Intro1.tsx
@@ -1,3 +1,5 @@
+import '../styles/Intro1.css';
+
 export default function Intro1() {
   return (
     <h1>Intro1</h1>
