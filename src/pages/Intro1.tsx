d880e0ae918cfe1e37966add4e346b80de534b0a
Jason Zhang <zjjc123@hotmail.com>
2023-02-18 15:21:37 -0800

    Video Player

--- src/pages/Intro1.tsx
+++ src/pages/Intro1.tsx
@@ -1,9 +1,22 @@
+import React, { useState, useRef } from "react";
+import ReactPlayer from "react-player";
 import Slider from "../components/ImageSlider";
+import { FaPlay, FaPause } from "react-icons/fa";
+import { GrRotateRight } from "react-icons/gr";
 import shen from "../assets/imgs/shen.jpeg";
+import demo from "../assets/videos/demo.mp4";
 
 import "../styles/Intro1.css";
 
 export default function Intro1() {
+  const [playing, setPlaying] = useState(false);
+
+  const playerRef = useRef<ReactPlayer>(null);
+
+  const togglePlaying = () => {
+    setPlaying(!playing);
+  };
+
   return (
     <div>
       <h1>Intro1</h1>
@@ -13,6 +26,16 @@ export default function Intro1() {
           { src: shen, alt: "shen" },
         ]}
       />
+      <div className="player-container">
+        <ReactPlayer ref={playerRef} url={demo} playing={playing} />
+        <button className="big-button" onClick={togglePlaying}>
+          {playing ? <FaPause /> : <FaPlay />}
+        </button>
+
+        <button className="big-button" onClick={() => playerRef.current ? playerRef.current?.seekTo(0, "seconds") : undefined}>
+          <GrRotateRight />
+        </button>
+      </div>
     </div>
   );
 }
