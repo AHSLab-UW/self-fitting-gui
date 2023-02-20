ad852b1162b0b5fd9d554b191549b6120757d898
Jason Zhang <zjjc123@hotmail.com>
2023-02-19 22:38:21 -0800

    Sidebar Styling

--- src/components/CollapsingSidebar.tsx
+++ src/components/CollapsingSidebar.tsx
@@ -29,21 +29,30 @@ const CollapsingSidebar = (props: Props) => {
         </div>
       </div>
       <div className="sidebar-body">
-        <Link to="/intro1">Intro 1</Link>
-        <div></div>
-        <Link to="/intro2">Intro 2</Link>
-        <div></div>
-        <Link to="/intro3">Intro 3</Link>
-        <div></div>
-        <Link to="/select">Select</Link>
-        <div></div>
-        <Link to="/fit">Fitting</Link>
-        <div></div>
-        <Link to="/adjust">Adjust</Link>
-        <div></div>
-        <Link to="/prompt">Prompt</Link>
-        <div></div>
-        <Link to="/finish">Finish</Link>
+        <Link className="link" to="/intro1">
+          Intro 1
+        </Link>
+        <Link className="link" to="/intro2">
+          Intro 2
+        </Link>
+        <Link className="link" to="/intro3">
+          Intro 3
+        </Link>
+        <Link className="link" to="/select">
+          Select
+        </Link>
+        <Link className="link" to="/fit">
+          Fitting
+        </Link>
+        <Link className="link" to="/adjust">
+          Adjust
+        </Link>
+        <Link className="link" to="/prompt">
+          Prompt
+        </Link>
+        <Link className="link" to="/finish">
+          Finish
+        </Link>
       </div>
       <div className="sidebar-footer" style={{ marginBottom: 100 }}>
         <p>UW Applied Hearing Science Lab</p>
