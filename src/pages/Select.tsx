034bc5d1a180ce7e5702cc42cb4ec950f37fa988
ZITONG0423 <81598813+ZITONG0423@users.noreply.github.com>
2023-02-21 15:06:11 -0800

    images

--- src/pages/Select.tsx
+++ src/pages/Select.tsx
@@ -2,7 +2,13 @@ import ImageSlider from "../components/ImageSlider";
 
 import shen from "../assets/imgs/shen.jpeg";
 import ear from "../assets/imgs/ear-03.png";
+import resturant from "../assets/imgs/resturant.png";
+import hiking from "../assets/imgs/hiking.png";
+import concert from "../assets/imgs/concert.png";
+
 import { NextButton } from "../components/NextButton";
+
+
 
 export default function Select() {
   return (
@@ -12,7 +18,14 @@ export default function Select() {
         images={[
           { src: shen, alt: "shen" },
           { src: ear, alt: "ear" },
+          { src: resturant, alt: "resturant" },
+          { src: hiking, alt: "hiking" },
+          { src: concert, alt: "concert" }
+
+          
         ]}
+
+        
       />
 
       <NextButton to="/fit" text="Next" />
