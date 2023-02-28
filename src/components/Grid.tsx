b1ba0780f38f614262094902cf57956dc24e5707
ZITONG0423 <81598813+ZITONG0423@users.noreply.github.com>
2023-02-27 20:48:19 -0800

    Style new (#12)
    * images
    
    * Update Grid.tsx
    
    * Update ImageSlider.css
    
    * side menu color
    
    * fix style
    
    ---------
    
    Co-authored-by: Jason Zhang <zjjc123@hotmail.com>

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -115,7 +115,7 @@ const Grid = ({ gainDelta }: Props) => {
   );
 
   useEffect(() => {
-    setGridSize((window.innerWidth / 3) * 2);
+    setGridSize((window.innerWidth /4.5) * 2);
     setCoordinates({
       x: 0,
       y: 0,
@@ -187,7 +187,7 @@ const Grid = ({ gainDelta }: Props) => {
           display: "grid",
           gridTemplateColumns: `repeat(3, 1fr)`,
           gridTemplateRows: `repeat(3, 1fr)`,
-          gap: `${gridSize / 75}px ${gridSize / 75}px`,
+          gap: `${gridSize / 70}px ${gridSize / 70}px`,
         }}
         onTouchStart={(e) => {
           setCoordinatesFromEvent(e.touches[0].clientX, e.touches[0].clientY);
@@ -215,9 +215,11 @@ const Grid = ({ gainDelta }: Props) => {
               height: "100%",
               border: `${gridSize / 150}px solid black`,
               boxSizing: "border-box",
+              background: "white",
             }}
           />
         ))}
+        
         <div
           className="dot"
           style={{
