f493179d48497374b8427de68bae55bd92b00ace
Jason Zhang <zjjc123@hotmail.com>
2023-03-24 15:40:26 -0700

    Customize

--- src/pages/Prompt.tsx
+++ src/pages/Prompt.tsx
@@ -4,6 +4,8 @@ export default function Prompt() {
   return (
     <div>
       <h3>Would you like to exit the app or customize another scene?</h3>
+      <NextButton to="/select" text="Customize" />
+      <div className="top-space"></div>
       <NextButton to="/finish" text="Exit" />
     </div>
   );
