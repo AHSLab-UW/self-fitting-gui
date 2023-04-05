9a4c3c7e70bddbf91f1fd491a120089c8e857733
AHS Lab <jiajunzh@uw.edu>
2023-04-04 19:19:54 -0700

    Fix path

--- src/pages/Prompt.tsx
+++ src/pages/Prompt.tsx
@@ -5,9 +5,9 @@ export default function Prompt() {
   return (
     <div>
       <h3>Would you like to exit the app or customize another scene?</h3>
-      <NextButton onclick={() => resetG()} to="/select" text="Customize" />
+      <NextButton onclick={() => resetG()} to="/fit-select" text="Customize" />
       <div className="top-space"></div>
-      <NextButton to="/finish" text="Exit" />
+      <NextButton onclick={() => resetG()} to="/finish" text="Exit" />
     </div>
   );
 }
