dae5500076e544d4742c925784f2c9a5aafd1355
Bertan Kursun <bertankursun@gmail.com>
2023-08-14 12:30:18 -0700

    adjusting the styling of the halfway popup

--- src/components/Halfway_grid.tsx
+++ src/components/Halfway_grid.tsx
@@ -8,14 +8,17 @@ type HalfwayProps = {
   handleContinue: () => void; 
 };
 
-const Halfway = ({ fadeIn, handleContinue }: HalfwayProps) => {
+
+const Halfway_grid = ({ fadeIn, handleContinue }: HalfwayProps) => {
   return (
-    <div className={`halfway_message`}>
+    <div className={`halfway_message_grid`}>
       <h3>You're halfway there - you're doing great!</h3>
       <h3>Keep going for a few more rounds!</h3> 
-      <button className="big-button" onClick={handleContinue}>Continue self-fitting</button>
+      <h3> Remember that you can use the volume control to adjust the overall sound level.</h3>
+
+      <button className="big-button" onClick={handleContinue}>Continue self-Fitting</button>
     </div>
   );
 };
 
-export default Halfway;
+export default Halfway_grid;
